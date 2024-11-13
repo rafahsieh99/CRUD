55a1e94e5ad7cc1a9bd80db6f5f9c799f387dc9a
rafahsieh99 <rafahsieh99@gmail.com>
2024-11-13 18:53:50 -0300

    Arreglo de script para el boton de votacion

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Escuchar clics en los botones de votación
     const voteButtons = document.querySelectorAll('.vote-button');
     voteButtons.forEach(button => {
-        button.addEventListener('click', (event) => {         
+        button.addEventListener('click', (event) => {
             const topicId = event.target.dataset.topicId;  // Obtener el ID del tema
 
             // Realizar la solicitud POST al servidor usando fetch
@@ -11,6 +11,14 @@ document.addEventListener("DOMContentLoaded", () => {
                 headers: {
                     'Content-Type': 'application/json',
                 },
+            })
+            .then(response => {
+                if (response.ok) {
+                    // Recargar la página para ver el voto actualizado
+                    window.location.reload();
+                } else {
+                    console.error("Error en la respuesta del servidor:", response.statusText);
+                }
             })
             .catch(error => {
                 console.error('Error en la solicitud AJAX:', error);
